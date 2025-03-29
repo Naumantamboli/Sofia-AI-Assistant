3e7108f4b5dcd17cf3d9fbf93bd838ea88e4f922
Nauman Tamboli <tambolinauman7@gmail.com>
2025-03-29 15:14:20 +0530

    changes made

--- musicLibrary.py
+++ musicLibrary.py
@@ -3,5 +3,4 @@ music = {
     "zehen" : "https://www.youtube.com/watch?v=1ml5HrQrdQI",
     "tumsehi" : "https://www.youtube.com/watch?v=Cb6wuzOurPc",
     "oSathi" : "https://www.youtube.com/watch?v=YuXLN23ZGQo"
-    
 }
